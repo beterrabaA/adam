a89553a5966e2c6acaf483c7f2e0e98e830c5da5
Beterraba <ls6182315@gmail.com>
2026-06-01 21:35:59 -0300

    initial commit

---
+++ src/main/java/com/test/adam/controller/RepositoryController.java
@@ -0,0 +1,8 @@
+package com.test.adam.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/repositories")
+public class RepositoryController {
+}
---
+++ src/test/java/com/test/adam/AdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.adam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
