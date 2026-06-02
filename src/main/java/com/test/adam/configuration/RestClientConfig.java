7fef1e2343382148b9810f824160673f5cf613f4
Beterraba <ls6182315@gmail.com>
2026-06-02 17:35:13 -0300

    Github client API configuration

---
+++ src/main/java/com/test/adam/configuration/RestClientConfig.java
@@ -0,0 +1,18 @@
+package com.test.adam.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient githubRestClient() {
+        return RestClient.builder()
+                .baseUrl("https://api.github.com")
+                .defaultHeader("Accept", "application/vnd.github+json")
+                .defaultHeader("X-GitHub-Api-Version", "2026-03-10")
+                .build();
+    }
+}
